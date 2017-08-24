4210603183180e585bde19714102beaca47ca21b
Julian Locke <julianlocke@hotmail.com>
2017-08-24 16:10:01 -0400

    Example of a fish ordering app in React.js

---
+++ README.md
@@ -0,0 +1,1 @@
+# Fish-Ordering-App
---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyAwlPJyk6UlXakfr6EKh7D_TxE171wcGFU",
+    authDomain: "catch-of-the-day-zardtar.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-zardtar.firebaseio.com",
+});
+
+export default base;
